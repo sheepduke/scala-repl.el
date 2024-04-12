22b1ddd8c13cb0067bb895bf078b4fb1847ee091
YUE Daian <sheepduke@gmail.com>
2024-04-12 20:18:02 +0800

    chore: add package file

---
+++ scala-repl-pkg.el
@@ -0,0 +1,11 @@
+(define-package "scala-repl" "20240412.2011" "Scala REPL Interaction"
+  '((emacs "29.1"))
+  :commit "6ecec8801933bfcda0089d0ff397437f795a1381"
+  :authors '(("Daian YUE" . "sheepduke@gmail.com"))
+  :maintainers '(("Daian YUE" . "sheepduke@gmail.com"))
+  :maintainer '("Daian YUE" . "sheepduke@gmail.com")
+  :keywords '("scala" "repl")
+  :url "https://github.com/sheepduke/scala-repl.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
